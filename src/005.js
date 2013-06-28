6453bef870453c8862a84d7707d1832e257c6580
Arsen Kuntic <guru.the.master@gmail.com>
2013-06-28 06:39:05 +0200

    Problem 005

--- src/005.js
+++ src/005.js
@@ -6,5 +6,24 @@
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 function main() {
-
+  var i = 1;
+  var j = 1;
+  var found = false;
+  while (i++) {
+    found = true;
+    j = 1;
+    for (j; j <= 20; j++) {
+      if (i % 19 !== 0) {
+        found = false;
+        break;
+      }
+      if (i % j !== 0) {
+        found = false;
+        break;
+      }
+    }
+    if (found === true) {
+      return i;
+    }
+  }
 }
