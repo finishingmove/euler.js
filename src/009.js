1093143757d5384c38e32c6fa2d97851778a0697
Arsen Kuntic <guru.the.master@gmail.com>
2013-07-03 03:22:51 +0200

    Problem 009 refactor

--- src/009.js
+++ src/009.js
@@ -9,14 +9,19 @@
 //There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 //Find the product abc.
 
-function main() {
+/**
+ *
+ * @param {Number} perimeter
+ * @returns {Number}
+ */
+(function pythagoreanTripletProduct(perimeter) {
   var c = 1;
-  for (var a = 1; a < 1000; a++) {
-    for (var b = 1; b < 1000; b++) {
+  for (var a = 1; a < perimeter; a++) {
+    for (var b = 1; b < perimeter; b++) {
       c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-      if (a + b + c === 1000) {
+      if (a + b + c === perimeter) {
         return a * b * c;
       }
     }
   }
-}
+})(1000);
