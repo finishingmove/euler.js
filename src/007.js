c9824f905f7c2ad2e33ae3541bbb3033efe679c5
Arsen Kuntic <guru.the.master@gmail.com>
2013-07-03 04:23:24 +0200

    Problem 007

---
+++ src/007.js
@@ -0,0 +1,32 @@
+//10001st prime
+//Problem 7
+//
+//By listing the first six prime numbers:
+//2, 3, 5, 7, 11, and 13,
+//we can see that the 6th prime is 13.
+//
+//What is the 10001st prime number?
+
+(function nthPrimeNumber(n) {
+  'use strict';
+  var primes = [2, 3];
+  var current;
+  var next;
+  var isPrime;
+  while (primes.length < n) {
+    current = primes[primes.length - 1];
+    next = next || current + 1;
+    isPrime = true;
+    for (var i = 2; i < next; i++) {
+      if (next % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(next);
+    }
+    next += 1;
+  }
+  return primes[primes.length - 1];
+})(10001);
--- src/008.js
+++ src/008.js
@@ -1,3 +1,5 @@
+//Largest product in a series
+//Problem 8
 //Find the greatest product of five consecutive digits in the 1000-digit number.
 //
 // 73167176531330624919225119674426574742355349194934
